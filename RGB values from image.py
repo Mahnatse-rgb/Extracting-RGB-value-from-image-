b76b3aa03c12384e514cf74457a0fc53f314125d
Mahnatse-rgb <mfchoung01@gmail.com>
2020-05-17 21:38:25 +0200

    first commit

---
+++ RGB values from image.py
@@ -0,0 +1,13 @@
+# Import image class of matplotlib
+import matplotlib.image as img
+
+# Read batman image and print dimensions
+image = img.imread('image.jpg')
+print(image.shape)
+
+# Store RGB values of all pixels in lists r, g and b
+for row in image:
+    for temp_r, temp_g, temp_b in row:
+        r.append(temp_r)
+        g.append(temp_g)
+        b.append(temp_b)
